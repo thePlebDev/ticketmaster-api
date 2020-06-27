e12ebc1e9da1821f88079cf62d5fa583ee19674f
Tristan Elliott <tristanelliott7@gmail.com>
2020-06-27 16:14:23 -0300

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import NavBar from '../NavBar'
+
+const App = ()=>{
+
+
+  return(
+    <div>
+    <NavBar />
+      <h1>App</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const NavBar = ()=>{
+
+
+  return(
+    <div>
+      <h1>the navbar</h1>
+    </div>
+  )
+}
+
+
+export default NavBar
