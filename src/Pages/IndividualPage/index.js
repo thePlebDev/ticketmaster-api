36f7eeb3c0d3ed1dc1049003353247130e01da9d
Tristan Elliott <tristanelliott7@gmail.com>
2020-07-03 21:51:20 -0300

    large screen styling

--- src/Pages/IndividualPage/index.js
+++ src/Pages/IndividualPage/index.js
@@ -7,12 +7,12 @@ import Modal from '../../components/Modal';
 
 
 const IndividualPage =({event})=>{
+  console.log(event)
 
 
+  const handleClick = (e)=>{
 
-  const handleClick = ()=>{
-
-    console.log(event)
+    console.log(e.target.id)
 
   }
 
@@ -22,16 +22,20 @@ const IndividualPage =({event})=>{
     <div className="indiv-page-container">{
       event?
         event.map((item,index)=>{
-        return  <div className='info-container' key={item.id}>
-                  <div className='info-date' >{item.dates.start.localDate}</div>
-                  <div className='info-title'>{item.name}</div>
-                  <div className='info-info'>{item._embedded.venues[0].name}</div>
-                  <div className='info-icon' onClick={()=>handleClick()}>{item.dates.status.code}</div>
-                  <button type="button" className="ticket-button">
-                    <a href={`${item._embedded.venues[0].url}`} rel='noreferrer' target="_blank" >See Ticket Info</a>
-                  </button>
-                  <div className='bottom-border'></div>
-                </div>
+        return  <div className='info-super-container'>
+                  <img className="large-screen-image" src={item.images[0].url} alt='band poster'/>
+                  <div className='info-container' key={item.id}>
+                    <div className='info-date' >{item.dates.start.localDate}</div>
+                    <div className='info-title'>{item.name}</div>
+                    <div className='info-info'>{item._embedded.venues[0].name}</div>
+                    <div className={item.dates.status.code ==='cancelled'?'info-icon-red':'info-icon' }
+                    onClick={(e)=>handleClick(e)}>{item.dates.status.code}</div>
+                    <button type="button" className="ticket-button">
+                      <a href={`${item._embedded.venues[0].url}`} rel='noreferrer' target="_blank" >Event Info</a>
+                    </button>
+                    <div className='bottom-border'></div>
+                  </div>
+              </div>
         })
       :''
     }
